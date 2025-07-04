b033e0b2f8053840ddd3f3bc7ad1888d741ed327
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 20:10:59 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Library Management</h1>
+      <p>This is a simple library management system.</p>
+    </div>
+  );
+}
+
+export default Home;
